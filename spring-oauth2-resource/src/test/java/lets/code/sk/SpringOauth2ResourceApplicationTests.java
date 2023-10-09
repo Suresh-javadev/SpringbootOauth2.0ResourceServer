a3de2f4265fa9f7f588234e1aac5433e174a0446
anssuresh <suresh.k@anssoft.in>
2023-10-09 23:38:21 +0530

    initial commit

---
+++ spring-oauth2-resource/src/test/java/lets/code/sk/SpringOauth2ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package lets.code.sk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
